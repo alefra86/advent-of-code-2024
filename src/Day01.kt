c8186d80366133dc0b9292711c0f1f813cfecfcf
Alessandro Franchini <alessandro.f@thoughtworks.com>
2024-12-03 17:29:39 +0100

    (day1) [AF] ✨ Implement day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,44 @@
+import kotlin.math.abs
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (first, second) = input.convertToSortedColumns()
+        return first.zip(second).sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbersCounter = mutableMapOf<Int, Int>()
+        input.forEach { line ->
+            val (_, col2) = line.toIntList()
+            numbersCounter[col2] = numbersCounter.getOrDefault(col2, 0) + 1
+        }
+
+        return input.sumOf { line ->
+            val (col1, _) = line.toIntList()
+            col1 * numbersCounter.getOrDefault(col1, 0)
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    "Part1" {
+        part1(testInput) shouldBe 11
+        measureAnswer { part1(input) }
+    }
+    "Part2" {
+        part2(testInput) shouldBe 31
+        measureAnswer { part2(input) }
+    }
 }
+
+fun String.toIntList() = this.split("\\s+".toRegex()).map { it.toInt() }
+
+fun List<String>.convertToSortedColumns(): Pair<List<Int>, List<Int>> =
+    map { it.toIntList() }
+        .let { rows ->
+            val left = rows.map { it[0] }.sorted()
+            val right = rows.map { it[1] }.sorted()
+            left to right
+        }
